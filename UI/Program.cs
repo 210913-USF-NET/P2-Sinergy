a704346a3690dd7838cf072d56b4fa1d114be9b6
Juniper Song <songminseonk@gmail.com>
2021-09-15 11:47:02 -0400

    scaffolding store app

---
+++ UI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Models;
+
+namespace UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to My Store!");
+            Customer newCustomer = new Customer();
+            newCustomer.Age = 1000;
+
+            StoreFront myStore = new StoreFront() {
+                Name = "MY Store",
+                Address = "123 NW abc ave, Portland, OR"
+            };
+            Console.WriteLine(myStore.ToString());
+            myStore.Name = Console.ReadLine();
+            Console.WriteLine(myStore.ToString());
+
+        }
+    }
+}
