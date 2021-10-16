07efbd00f03f7d225123b12dba5a7ddb7c187e0b
Siavash Nezhad <90288482+TheSeaOfAsh@users.noreply.github.com>
2021-10-16 13:39:28 -0500

    User model completed

---
+++ Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models
+{
+    public class User
+    {
+        public User() { }
+
+        public User(int UserID) : this()
+        {
+            this.UserID = UserID;
+        }
+
+        public User(int UserID, string email) : this(UserID)
+        {
+            this.email = email;
+        }
+
+        public User(int UserID, string email, string account) : this(UserID, email)
+        {
+            this.account = account;
+        }
+
+        public User(int UserID, string email, string account, bool admin) : this(UserID, email, account) {}
+
+        //Property
+        public int UserID { get; set; }
+
+        public string email { get; set; }
+
+        public string account { get; set; }
+
+        public bool admin { get; set; }
+    }
+}
