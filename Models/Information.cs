40f712d5a3e9a9ab5affede20fca00ce46553d11
Siavash Nezhad <90288482+TheSeaOfAsh@users.noreply.github.com>
2021-10-16 14:09:21 -0500

    Information model updated

---
+++ Models/Information.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace Information
+{
+    public class Information
+    {
+        public Information(){}
+        public Information(int InformationID) : this()
+        {
+            this.InformationID = InformationID;
+        }
+        public int InformationID {get; set;}
+        public string Genre { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public int Duration { get; set; } //will need to convert from milliseconds
+        public string Country { get; set; }
+    }
+}
