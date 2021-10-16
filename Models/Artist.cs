2026d8fbb54042d7d182f958d1b28eead4bcbcd5
Siavash Nezhad <90288482+TheSeaOfAsh@users.noreply.github.com>
2021-10-16 14:11:39 -0500

    models finished

---
+++ Models/Artist.cs
@@ -0,0 +1,8 @@
+namespace Models
+{
+    public class artist
+    {
+        public string ArtistName { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ Models/Genre.cs
@@ -0,0 +1,8 @@
+namespace Models
+{
+    public class Genre
+    {
+        public string GenreType { get; set; }
+        public int Count { get; set; }
+    }
+}
