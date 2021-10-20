de5c6d6366e71635955d790ddef52d670657e52f
Garrett Huon <ghuon407@gmail.com>
2021-10-19 20:44:24 -0400

    added logging

--- DL/DBRepo.cs
+++ DL/DBRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,21 @@ namespace DL
 {
     public class DBRepo : IRepo
     {
+        private CacophanyDBContext _context;
+        public DBRepo(CacophanyDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Playlist> AddPlaylistAsync(Playlist playlist)
+        {
+            await _context.AddAsync(playlist);
+            await _context.SaveChangesAsync();
+            _context.ChangeTracker.Clear();
+            return playlist;
+
+        }
+
+
     }
 }
