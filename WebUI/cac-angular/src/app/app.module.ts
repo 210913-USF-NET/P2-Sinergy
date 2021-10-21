28ba3cf26046008d417be66c1ec04bd091a66dfa
Peiferinc <peiferjustin96@gmail.com>
2021-10-21 02:31:11 -0500

    created login button but cannot get it to show up

--- WebUI/cac-angular/src/app/app.module.ts
+++ WebUI/cac-angular/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PlaylistMakerComponent } from './playlist-maker/playlist-maker.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
+import { LoginButtonComponent } from './components/login-button/login-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlaylistMakerComponent,
     NavBarComponent,
-    LoginComponent
+    LoginComponent,
+    LoginButtonComponent
   ],
   imports: [
     BrowserModule,
