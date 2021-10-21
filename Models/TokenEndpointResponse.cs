46b8b163c712b494b573afd0090a701dd5ab4e64
Philip Catullo <philip.catullo@revature.net>
2021-10-20 21:33:07 -0400

    Added Token Auth Spotify
    Basic Token Authorization for Spotify API

---
+++ Models/TokenEndpointResponse.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class TokenEndpointResponse
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; }
+        
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; }
+        [JsonPropertyName("expires_in")]
+        public int ExpiresIn { get; set; }
+    }
+}
